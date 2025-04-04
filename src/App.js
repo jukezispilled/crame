ca26aa88f0801f899c3588d1cfc86a64cd90ac4c
agorapilled <johngardner13@outlook.com>
2025-04-03 23:03:11 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ function App() {
               <button className='p-1 text-xs md:text-base m-1 bg-zinc-400'>
                 Copy
               </button>
-              <span className='p-1 text-[9px] md:text-base font-semibold'>uploading...</span>
+              <span className='p-1 text-[9px] md:text-base font-semibold'>9TWav8cMJdR4urBr3AJdDpPKshsgRHSxh8KhPoLnpump</span>
             </Window>
           </div>
           <Window>
